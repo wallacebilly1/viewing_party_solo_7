573850012ac3cf7e91246ed2bfe9514a5fda8af6
Billy Wallace <wallace.billy1@gmail.com>
2024-05-09 07:59:03 -0600

    Update schema with new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_13_193418) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_09_133932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_13_193418) do
     t.string "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "movie_id"
+    t.integer "movie_duration"
   end
 
   add_foreign_key "user_parties", "users"
