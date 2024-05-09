5a7c02a65e1dfcb017bef41347b30d3aff78bf50
Billy Wallace <wallace.billy1@gmail.com>
2024-05-09 14:13:23 -0600

    Create feature tests for US7

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Show User Dashboard', type: :feature do
-  describe 'When user visits "/users/:id"' do
+  describe 'When user visits their user dashboard"' do
     before(:each) do
-      @user1 = User.create!(name: 'Tommy', email: 'tommy@email.com')
+      @user1 = User.create!(name: 'Sam', email: 'sam@email.com')
+      @user2 = User.create!(name: 'Tommy', email: 'tommy@email.com')
+      @party1 = ViewingParty.create!(date: "06/30/2024", start_time: "07:25", duration: 200, movie_id: 121, movie_duration: 179)
+      @party2 = ViewingParty.create!(date: "07/15/2024", start_time: "06:35", duration: 220, movie_id: 122, movie_duration: 201)
+      UserParty.create!(user_id: @user1.id, viewing_party_id: @party1.id, host: true)
+      UserParty.create!(user_id: @user2.id, viewing_party_id: @party1.id, host: false)
+
+      UserParty.create!(user_id: @user1.id, viewing_party_id: @party2.id, host: false)
+      UserParty.create!(user_id: @user2.id, viewing_party_id: @party2.id, host: true)
 
       visit user_path(@user1)
     end
@@ -14,6 +22,43 @@ RSpec.describe 'Show User Dashboard', type: :feature do
       click_button 'Discover Movies'
 
       expect(current_path).to eq(user_discover_index_path(@user1))
+    end
+
+    it 'They see all viewing parties they have been invited to or are hosting' do
+      expect(page).to have_content("Lord of the Rings: The Two Towers")
+      expect(page).to have_content("Lord of the Rings: The Return of the King")
+    end
+
+    it 'They see all information about a viewing party they are hosting' do
+      within "#hosting" do
+        expect(page).to have_css("img")
+        expect(page).to have_content("The Lord of the Rings: The Two Towers")
+        expect(page).to have_link("The Lord of the Rings: The Two Towers")
+        expect(page).to have_content("06/30/2024 - 07:25AM")
+        expect(page).to have_content("Host: Sam")
+        within "#invitees" do
+          expect(page).to have_content("Sam Tommy")
+        end
+      end
+    end
+
+    it 'They see all information about a viewing party they are invited to' do
+      within "#attending" do
+        expect(page).to have_css("img")
+        expect(page).to have_content("The Lord of the Rings: The Return of the King")
+        expect(page).to have_link("The Lord of the Rings: The Return of the King")
+        expect(page).to have_content("07/15/2024 - 06:35AM")
+        expect(page).to have_content("Host: Tommy")
+        within "#invitees" do
+          expect(page).to have_content("Tommy Sam")
+          expect(find(:css, ".bold_user").text).to have_content("Sam")
+        end
+      end
+    end
+
+    it 'They are taken to the movie show page when clicking on the title of a movie' do
+      click_on("The Lord of the Rings: The Two Towers")
+      expect(current_path).to eq(user_movie_path(@user1, 122))
     end
   end
 end
