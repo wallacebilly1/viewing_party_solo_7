09bb0047a70bf90c2b1f10bfb305f5fbba1186ae
Billy Wallace <wallace.billy1@gmail.com>
2024-05-20 13:19:16 -0600

    Added BCrypt and updated user model, database, and spec to match password being required

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
    validates_presence_of :name, :email
    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+   validates_presence_of :password
+   validates_presence_of :password_confirmation
 
    has_many :user_parties
    has_many :viewing_parties, through: :user_parties
+
+   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,17 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
+    it { should validate_presence_of(:password)}
+    it { should have_secure_password }
     it { should allow_value('something@something.something').for(:email) }
     it { should_not allow_value('something somthing@something.something').for(:email) }
     it { should_not allow_value('something.something@').for(:email) }
     it { should_not allow_value('something').for(:email) }
-
+    it "Does not return the password after creating a user" do
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end
   end
 
   describe 'associations' do
