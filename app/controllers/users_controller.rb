05a6a03eddb5609117d20c398536678c4bfe6adf
Billy Wallace <wallace.billy1@gmail.com>
2024-05-20 14:32:11 -0600

    Add functionality for log in page and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,31 @@ class UsersController < ApplicationController
 
   def create
     @user = user_params
-    @user[:username] = @user[:username].downcase
+    @user[:email] = @user[:email].downcase
     @new_user = User.new(user_params)
+
     if @new_user.save
       flash[:success] = 'Successfully Created New User'
       redirect_to user_path(@new_user)
     else
       flash[:error] = "#{error_message(@new_user.errors)}"
       redirect_to register_user_path
+    end
+  end
+
+  def login_form
+
+  end
+
+  def login_user
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}!"
+      redirect_to user_path(user)
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      render :login_form
     end
   end
 
---
+++ spec/features/users/login_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Logging In" do
+  it "can log in with valid credentials" do
+    user = User.create!(name: "Meg", email: "meg@test.com", password: "test", password_confirmation: "test")
+
+    visit root_path
+
+    click_on "Log In Here"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+
+    click_on "Log In"
+    log_in_user = User.last
+
+    expect(current_path).to eq(user_path(log_in_user))
+
+    expect(page).to have_content("Welcome, #{user.name}")
+  end
+
+  it "cannot log in with bad credentials" do
+    user = User.create!(name: "Meg", email: "meg@test.com", password: "test", password_confirmation: "test")
+  
+    visit login_path
+  
+    fill_in :email, with: user.email
+    fill_in :password, with: "123"
+  
+    click_on "Log In"
+  
+    expect(current_path).to eq(login_path)
+  
+    expect(page).to have_content("Sorry, your credentials are bad.")
+  end
+end
