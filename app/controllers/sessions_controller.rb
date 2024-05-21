35a5e8112bb9a78a87be531a019cf7b88947ba29
Billy Wallace <wallace.billy1@gmail.com>
2024-05-21 13:58:29 -0600

    Added location cookie to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
       session[:user_id] = user.id
+      cookies[:location] = params[:location]
       flash[:success] = "Welcome, #{user.name}!"
       if user.admin?
         redirect_to admin_dashboard_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = user_params
-    @user[:email] = @user[:email].downcase
-    @new_user = User.new(user_params)
+    user = user_params
+    user[:email] = user[:email].downcase
+    new_user = User.new(user_params)
 
-    if @new_user.save
+    if new_user.save
       flash[:success] = 'Successfully Created New User'
-      redirect_to user_path(@new_user)
+      redirect_to user_path(new_user)
     else
-      flash[:error] = "#{error_message(@new_user.errors)}"
+      flash[:error] = "#{error_message(new_user.errors)}"
       redirect_to register_user_path
     end
   end
