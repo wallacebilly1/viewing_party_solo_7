5a279c75843949c47aef55e1209441621b6e111b
Billy Wallace <wallace.billy1@gmail.com>
2024-05-22 06:54:14 -0600

    Configure landing page to show different fields for logged in users

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Root Page, Welcome Index', type: :feature do
-  describe 'When a user visits the root path "/"' do
+  describe 'When a user visits the root path "/" and logs in' do
     before(:each) do
     @user_1 = User.create!(name: 'Sam', email: 'sam_t@email.com', password: "password", password_confirmation: "password")
     @user_2 = User.create!(name: 'Tommy', email: 'tommy_t@gmail.com', password: "password", password_confirmation: "password")
+
+    visit login_path
+
+    fill_in :email, with: @user_1.email
+    fill_in :password, with: @user_1.password
+
+    click_on "Log In"
 
     visit root_path
     end
@@ -14,8 +21,8 @@ RSpec.describe 'Root Page, Welcome Index', type: :feature do
       expect(page).to have_link('Home')
     end
 
-    it 'They see button to create a New User' do
-      expect(page).to have_selector(:link_or_button, 'Create New User')
+    it 'They do not a see button to create a New User' do
+      expect(page).to_not have_selector(:link_or_button, 'Create New User')
     end
 
     it "They see a list of existing users, which links to the individual user's dashboard" do
@@ -25,6 +32,30 @@ RSpec.describe 'Root Page, Welcome Index', type: :feature do
         expect(page).to have_link("#{User.first.email}", href: "users/#{User.first.id}")
         expect(page).to have_link("#{User.last.email}", href: "users/#{User.last.id}")
       end   
+    end
+
+    it "They see a link to go back to the landing page (present at the top of all pages)" do
+      expect(page).to have_link("Home")
+    end
+  end
+
+  describe 'When a user visits the root path "/" and is not logged in' do
+    before(:each) do
+    @user_1 = User.create!(name: 'Sam', email: 'sam_t@email.com', password: "password", password_confirmation: "password")
+    @user_2 = User.create!(name: 'Tommy', email: 'tommy_t@gmail.com', password: "password", password_confirmation: "password")
+
+    visit root_path
+    end
+
+    it 'They see button to create a New User' do
+      expect(page).to have_selector(:link_or_button, 'Create New User')
+    end
+
+    it "They do not see a list of existing users, which links to the individual user's dashboard" do
+      expect(page).to_not have_content(User.first.email)
+      expect(page).to_not have_content(User.last.email)
+      expect(page).to_not have_link("#{User.first.email}", href: "users/#{User.first.id}")
+      expect(page).to_not have_link("#{User.last.email}", href: "users/#{User.last.id}")
     end
 
     it "They see a link to go back to the landing page (present at the top of all pages)" do
